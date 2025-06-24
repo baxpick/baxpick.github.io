0c38b0840dae673590ce0b1767a83115511d93ad
Bax Picker <baxpicker@gmail.com>
2025-06-24 12:00:19 +0200

    Test import

---
+++ 3013/C/R/osmobito/images/sfore/bg_constants.h
@@ -0,0 +1,10 @@
+// Data created with Img2CPC - (c) Retroworks - 2007-2017
+// Palete constants
+#define G_TILE_PALETTE_OFF    0
+#define G_TILE_PALETTE_SIZE   0
+
+// Tile g_tile_bg - 320x200 pixels, 80x200 bytes.
+#define G_TILE_BG_OFF        0
+#define G_TILE_BG_SIZE       16384
+#define G_TILE_BG_W          80
+#define G_TILE_BG_H          200
